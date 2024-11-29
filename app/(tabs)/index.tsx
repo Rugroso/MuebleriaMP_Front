ad159961843c79fde21f1fad76af11ef9bb9f7ec
BarcosyPizzas <67290371+BarcosyPizzas@users.noreply.github.com>
2024-11-29 01:59:25 -0800

    ASAP ROCKY

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
-import { View, Text, ScrollView, Modal, Pressable, TextInput } from 'react-native';
+import { View, Text, ScrollView, Modal, Pressable, Alert, TextInput } from 'react-native';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { useState, useEffect } from 'react';
 import { generarReporteCompras, generarReporteVentas, generarReporteMuebles, generarReporteCredito } from '../../utils/pdfGenerator';
-import { Alert } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const fetchData = async (endpoint: string) => {
   try {
@@ -27,11 +27,15 @@ export default function HomeScreen() {
   const [mueblesFormData, setMueblesFormData] = useState({ sucursal: '' });
   const [ventaDelDia, setVentaDelDia] = useState<{ VENTAS_DEL_DIA : string, CANTIDAD_TOTAL_VENTA: string }[]>([]);
   const [inventarioTotal, setInventarioTotal] = useState<{ Total_Productos : string, Valor_Total: string }[]>([]);
+  const [showDatePicker, setShowDatePicker] = useState(true);
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+  const [showDatePickerVentas, setShowDatePickerVentas] = useState(false);
+  const [selectedDateVentas, setSelectedDateVentas] = useState<Date | undefined>(undefined);
 
 
   const fetchViewVentaDia = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/ventadeldia`);
+      const response = await fetch(`http://http://192.168.100.7/ventadeldia`);
       if (!response.ok) throw new Error(`Error al obtener datos de la venta del dia`);
       const data = await response.json();
       if(Array.isArray(data)) {
@@ -48,7 +52,7 @@ export default function HomeScreen() {
 
   const fetchInventarioTotal = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/inventariototal`);
+      const response = await fetch(`http://http://192.168.100.7/inventariototal`);
       if (!response.ok) throw new Error(`Error al obtener datos de la venta del dia`);
       const data = await response.json();
       if(Array.isArray(data)) {
@@ -173,7 +177,8 @@ export default function HomeScreen() {
         <Pressable onPress={async () => {
           try {
             // Obtener datos de clientes en crédito
-            const response = await fetch('http://192.168.100.7/clientecredito'); // Asegúrate de que este endpoint existe
+            const endpointlocal = "http://192.168.100.7"
+            const response = await fetch(`${endpointlocal}/clientecredito`); // Asegúrate de que este endpoint existe
             const datosAPI = await response.json();
             console.log(datosAPI);
             // Formateamos los datos
@@ -221,63 +226,79 @@ export default function HomeScreen() {
             <View className='bg-stone-900 p-6 rounded-lg w-[90%]'>
               <Text className='text-xl font-bold mb-4 text-white'>Generar Reporte de Compras</Text>
               
-              <TextInput
-                className='border border-stone-700 p-2 rounded-lg mb-4'
-                placeholder="Fecha (MM/YYYY)"
-                placeholderTextColor={'gray'}
-                value={formData.fecha}
-                onChangeText={(text) => setFormData({...formData, fecha: text})}
-              />
+              <Pressable onPress={() => setShowDatePicker(true)}>
+                <Text className='text-white'>Seleccionar Fecha</Text>
+              </Pressable>
 
-              <View className='flex-row justify-center space-x-4 w-full'>
-                <Pressable
-                  className='bg-red-500 px-4 py-2 rounded-lg mr-3'
-                  onPress={() => setModalVisible(false)}
-                >
-                  <Text className='text-white'>Cancelar</Text>
-                </Pressable>
-                
-                <Pressable
-                  className='bg-blue-500 px-4 py-2 rounded-lg'
-                  onPress={async () => {
-                    try {
-                      // Extraer mes y año de la fecha
-                      const [mes, anio] = formData.fecha.split('/');
-                      
-                      // Construir el query string
-                      const queryString = `comprasmensuales?mes=${mes}&anio=${anio}`;
-                      
-                      // Obtener datos con el nuevo query string
-                      const datosAPI = await fetchData(queryString);
-                      console.log(datosAPI); // Verifica la estructura de datosAPI
-                      
-                      // Formateamos los datos
-                      const datosReporte = {
-                        fecha: formData.fecha,
-                        reportes: datosAPI.map((reporte: { Distribuidor: any; Mueble: any; CANTIDAD: any; Costo_Unitario: any; Costo_Total: any; }) => ({
-                          distribuidor: reporte.Distribuidor,
-                          mueble: reporte.Mueble,
-                          cantidad: reporte.CANTIDAD,
-                          costoUnitario: reporte.Costo_Unitario,
-                          costoTotal: reporte.Costo_Total
-                        }))
-                      };
-
-                      const success = await generarReporteCompras(datosReporte);
-                      
-                      if (success) {
-                        Alert.alert('Éxito', 'PDF generado correctamente');
-                        setModalVisible(false);
+              {showDatePicker && (
+                <View>
+                  <DateTimePicker
+                    value={selectedDate || new Date()}
+                    mode="date"
+                    display="default"
+                    onChange={(event, date) => {
+                      if (event.type === 'set' && date) {
+                        setSelectedDate(date);
                       }
-                    } catch (error) {
-                      console.error('Error:', error);
-                      Alert.alert('Error', 'No se pudo generar el reporte');
-                    }
-                  }}
-                >
-                  <Text className='text-white'>Generar PDF</Text>
-                </Pressable>
-              </View>
+                      setShowDatePicker(true);
+                    }}
+                  />
+                  <View className='flex-row justify-between w-full mt-4'>
+                    <Pressable
+                      className='bg-blue-500 px-4 py-2 rounded-lg'
+                      onPress={async () => {
+                        if (!selectedDate) {
+                          Alert.alert('Error', 'Por favor, selecciona una fecha.');
+                          return;
+                        }
+                        try {
+                          // Extraer mes y año de la fecha seleccionada
+                          const mes = selectedDate.getMonth() + 1;
+                          const anio = selectedDate.getFullYear();
+                          
+                          // Construir el query string
+                          const queryString = `comprasmensuales?mes=${mes}&anio=${anio}`;
+                          
+                          // Obtener datos con el nuevo query string
+                          const datosAPI = await fetchData(queryString);
+                          console.log(datosAPI); // Verifica la estructura de datosAPI
+                          
+                          // Formateamos los datos
+                          const datosReporte = {
+                            fecha: `${mes}/${anio}`,
+                            reportes: datosAPI.map((reporte: { Distribuidor: any; Mueble: any; CANTIDAD: any; Costo_Unitario: any; Costo_Total: any; }) => ({
+                              distribuidor: reporte.Distribuidor,
+                              mueble: reporte.Mueble,
+                              cantidad: reporte.CANTIDAD,
+                              costoUnitario: reporte.Costo_Unitario,
+                              costoTotal: reporte.Costo_Total
+                            }))
+                          };
+
+                          const success = await generarReporteCompras(datosReporte);
+                          
+                          if (success) {
+                            Alert.alert('Éxito', 'PDF generado correctamente');
+                            setModalVisible(false);
+                          }
+                        } catch (error) {
+                          console.error('Error:', error);
+                          Alert.alert('Error', 'No se pudo generar el reporte');
+                        }
+                      }}
+                    >
+                      <Text className='text-white'>Generar PDF</Text>
+                    </Pressable>
+
+                    <Pressable onPress={() => {
+                      setSelectedDate(undefined);
+                      setModalVisible(false);
+                    }}>
+                      <Text className='text-red-500'>Cancelar</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              )}
             </View>
           </View>
         </Modal>
@@ -291,60 +312,79 @@ export default function HomeScreen() {
           <View className='flex-1 justify-center items-center bg-black/50'>
             <View className='bg-stone-900 p-6 rounded-lg w-[90%]'>
               <Text className='text-xl font-bold mb-4 text-white'>Generar Reporte de Ventas</Text>
-              <TextInput
-                className='border border-stone-700 p-2 rounded-lg mb-4'
-                placeholder="Fecha (MM/YYYY)"
-                placeholderTextColor={'gray'}
-                value={ventasFormData.fecha}
-                onChangeText={(text) => setVentasFormData({...ventasFormData, fecha: text})}
-              />
-              <View className='flex-row justify-center space-x-4 w-full'>
-                <Pressable
-                  className='bg-red-500 px-4 py-2 rounded-lg mr-3'
-                  onPress={() => setVentasModalVisible(false)}
-                >
-                  <Text className='text-white'>Cancelar</Text>
-                </Pressable>
-                <Pressable
-                  className='bg-blue-500 px-4 py-2 rounded-lg'
-                  onPress={async () => {
-                    try {
-                      // Extraer mes y año de la fecha
-                      const [mes, anio] = ventasFormData.fecha.split('/');
-                      
-                      // Construir el query string
-                      const queryString = `ventamensual?mes=${mes}&anio=${anio}`;
-                      
-                      // Obtener datos con el nuevo query string
-                      const datosAPI = await fetchData(queryString);
-                      console.log(datosAPI);
-                      // Formateamos los datos
-                      const datosReporte = {
-                        fecha: ventasFormData.fecha,
-                        ventas: datosAPI.map((reporte: { Mueble: any; CANTIDAD: any; Costo_Unitario: any; Costo_Total: any; Victima: any; }) => ({
-                          nombre: reporte.Victima,
-                          mueble: reporte.Mueble,
-                          cantidad: reporte.CANTIDAD,
-                          costoUnitario: reporte.Costo_Unitario,
-                          costoTotal: reporte.Costo_Total
-                        }))
-                      };
+              
+            
+                <Text className='text-white'>Seleccionar Fecha</Text>
+          
 
-                      const success = await generarReporteVentas(datosReporte);
-                      
-                      if (success) {
-                        Alert.alert('Éxito', 'PDF generado correctamente');
-                        setVentasModalVisible(false);
+              {showDatePickerVentas && (
+                <View>
+                  <DateTimePicker
+                    value={selectedDateVentas || new Date()}
+                    mode="date"
+                    display="default"
+                    onChange={(event, date) => {
+                      if (event.type === 'set' && date) {
+                        setSelectedDateVentas(date);
                       }
-                    } catch (error) {
-                      console.error('Error:', error);
-                      Alert.alert('Error', 'No se pudo generar el reporte');
-                    }
-                  }}
-                >
-                  <Text className='text-white'>Generar PDF</Text>
-                </Pressable>
-              </View>
+                    }}
+                  />
+                  <View className='flex-row justify-between w-full mt-4'>
+                    <Pressable
+                      className='bg-blue-500 px-4 py-2 rounded-lg'
+                      onPress={async () => {
+                        if (!selectedDateVentas) {
+                          Alert.alert('Error', 'Por favor, selecciona una fecha.');
+                          return;
+                        }
+                        try {
+                          // Extraer mes y año de la fecha seleccionada
+                          const mes = selectedDateVentas.getMonth() + 1;
+                          const anio = selectedDateVentas.getFullYear();
+                          
+                          // Construir el query string
+                          const queryString = `ventamensual?mes=${mes}&anio=${anio}`;
+                          
+                          // Obtener datos con el nuevo query string
+                          const datosAPI = await fetchData(queryString);
+                          console.log(datosAPI); // Verifica la estructura de datosAPI
+                          
+                          // Formateamos los datos
+                          const datosReporte = {
+                            fecha: `${mes}/${anio}`,
+                            ventas: datosAPI.map((reporte: { Mueble: any; CANTIDAD: any; Costo_Unitario: any; Costo_Total: any; Victima: any; }) => ({
+                              nombre: reporte.Victima,
+                              mueble: reporte.Mueble,
+                              cantidad: reporte.CANTIDAD,
+                              costoUnitario: reporte.Costo_Unitario,
+                              costoTotal: reporte.Costo_Total
+                            }))
+                          };
+
+                          const success = await generarReporteVentas(datosReporte);
+                          
+                          if (success) {
+                            Alert.alert('Éxito', 'PDF generado correctamente');
+                            setVentasModalVisible(false);
+                          }
+                        } catch (error) {
+                          console.error('Error:', error);
+                          Alert.alert('Error', 'No se pudo generar el reporte');
+                        }
+                      }}
+                    >
+                      <Text className='text-white'>Generar PDF</Text>
+                    </Pressable>
+
+                    <Pressable onPress={() => {
+                      setSelectedDateVentas(undefined);
+                      setVentasModalVisible(false);
+                    }}>
+                      <Text className='text-red-500'>Cancelar</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              )}
             </View>
           </View>
         </Modal>
